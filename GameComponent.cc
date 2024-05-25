cc52568f251b052f75868eae13d99217d8bc8df4
ryann66 <ryannelson6@hotmail.com>
2024-05-24 23:36:32 -0700

    GameComponent TODO defs

--- GameComponent.cc
+++ GameComponent.cc
@@ -2,4 +2,41 @@
 
 namespace game {
 
+bool collide(Ray& ray, LineSegment& line, Collision* out) {
+
+}
+
+bool Laser::hitboxClicked(int x, int y) {
+
+}
+
+bool Laser::collide(Ray& ray, Collision* out) {
+	// lasers don't block lasers
+	return false;
+}
+
+bool Target::hitboxClicked(int x, int y) {
+
+}
+
+bool Target::collide(Ray& ray, Collision* out) {
+
+}
+
+bool Blocker::hitboxClicked(int x, int y) {
+
+}
+
+bool Blocker::collide(Ray& ray, Collision* out) {
+
+}
+
+bool Mirror::hitboxClicked(int x, int y) {
+
+}
+
+bool Mirror::collide(Ray& ray, Collision* out) {
+
+}
+
 }  // namespace game
